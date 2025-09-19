f0b91f526697100a854dafe2be269d309ea3aa93
unknown <uwumina56@gmail.com>
2025-09-19 16:03:28 +0500

    docker, postgreSQL

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const tasksRouter = require('./routes/tasks');
@@ -5,7 +6,10 @@ const path = require('path');
 
 app.use(express.json());
 app.use('/api/tasks', tasksRouter);
-
+app.use(express.static(path.join(__dirname, '../ui')));
+app.get(/.*/, (req, res) => {
+  res.sendFile(path.join(__dirname, '../ui/index.html'));
+});
 //Сервер
 const PORT = 3000;
 app.listen(PORT, () => {
--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,61 +1,59 @@
 const express = require('express');
 const router = express.Router();
-const fs = require('fs');
+// const fs = require('fs');
 const path = require('path');
-const tasksFile = path.join(__dirname, '../data/tasks.json');
+// const tasksFile = path.join(__dirname, '../data/tasks.json');
+const pool = require('../db');
+const { text } = require('stream/consumers');
 
-//Чтение и запись задач, обработка ошибок
-const readTasks = () => {
-    try {
-      const data = fs.readFileSync(tasksFile, 'utf-8');
-      return JSON.parse(data);
-    } catch (error) {
-      console.error('Ошибка при чтении задач:', error);
-      return []; 
-    }
-  };
-  const writeTasks = (tasks) => {
-    try {
-      fs.writeFileSync(tasksFile, JSON.stringify(tasks, null, 2));
-    } catch (error) {
-      console.error('Ошибка при записи задач:', error);
-    }
-  };
-//Получить
-router.get('/', (req, res) => {
-  const tasks = readTasks();
-  res.json(tasks);
+router.get('/', async (req, res)=>{
+  try{
+    const result=await pool.query('SELECT * FROM tasks ORDER BY id ASC');
+    res.json(result.rows);
+  }catch(error){
+    console.error('Ошибка при получении задач: ', error);
+    res.status(500).json({error: 'Ошибка сервера'})
+  }
 });
-//Добавить
-router.post('/', (req, res) => {
-  const tasks = readTasks();
-  const newTask = {
-    id: Date.now().toString(),
-    text: req.body.text || '',
-    completed: false,
-  };
-  tasks.push(newTask);
-  writeTasks(tasks);
-  res.status(201).json(newTask);
+//добавить
+router.post('/', async (req, res)=>{
+  try{
+    const { text }= req.body;
+    if(!text || text.trim()===''){
+      return res.status(400).json({error:'Текст задачи обязателен'});
+    }
+    const result=await pool.query('INSERT INTO tasks (text, completed) VALUES ($1, $2) RETURNING*', [text, false]
+    );
+    res.status(201).json(result.rows[0]);
+  }catch(error){
+    console.error('Ошибка при добавлении задачи', error);
+    res.status(500).json({error: 'Ошибка сервера'});
+  }
 });
 //Удалить 
-router.delete('/:id', (req, res) => {
-  const tasks = readTasks();
-  const filteredTasks = tasks.filter(task => task.id !== req.params.id);
-  writeTasks(filteredTasks);
-  res.status(204).end();
+router.delete('/:id', async (req, res) => {
+  try{
+    const { id }= req.params;
+    await pool.query(`DELETE FROM tasks WHERE id = $1`, [id]);
+    res.status(204).end();
+  }catch(error){
+    console.error('Ошибка при удалении задачи', error);
+    res.status(500).json({error: 'Ошибка сервера'});
+  }
 });
 //Обновить 
-router.put('/:id', (req, res) => {
-  const tasks = readTasks();
-  const updatedTasks = tasks.map(task => {
-    if (task.id === req.params.id) {
-      return { ...task, ...req.body };
-    }
-    return task;
-  });
-  writeTasks(updatedTasks);
-  const updatedTask = updatedTasks.find(t => t.id === req.params.id);
-  res.json(updatedTask);
+router.put('/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { text, completed } = req.body;
+    const result = await pool.query(
+      'UPDATE tasks SET text = $1, completed = $2 WHERE id = $3 RETURNING *',
+      [text || '', completed, id]
+    );
+    res.json(result.rows[0]);
+  } catch (error) {
+    console.error('Ошибка при обновлении задачи', error);
+    res.status(500).json({ error: 'Ошибка сервера' });
+  }
 });
 module.exports = router;
